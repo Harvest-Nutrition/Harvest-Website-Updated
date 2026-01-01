d7e9d6df63e89c1c09eab71f79f9cdd3478fd2dc
matthewlosito <mlosito@ufl.edu>
2026-01-01 03:20:14 -0500

    Add env config to next.config.ts for Amplify SSR

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
+  env: {
+    WAITLIST_AWS_KEY: process.env.WAITLIST_AWS_KEY,
+    WAITLIST_AWS_SECRET: process.env.WAITLIST_AWS_SECRET,
+    NEXT_PUBLIC_REGION: process.env.NEXT_PUBLIC_REGION,
+    NEXT_PUBLIC_TABLE_NAME: process.env.NEXT_PUBLIC_TABLE_NAME,
+  },
 };
 
 export default nextConfig;
