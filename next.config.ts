e32c4c00187257c4d0da20b4bc9629a2fb495fb2
matthewlosito <mlosito@ufl.edu>
2026-01-04 16:21:58 -0500

    fixed amplify errors

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: {
-    buildActivityPosition: 'bottom-right',
-  },
   experimental: {
     serverActions: {
       bodySizeLimit: '2mb',
